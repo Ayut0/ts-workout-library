b11efd580ceea8d0c742dbd25d19333eb67059ca
Yuto Yamakita <yuvto.0420@gmail.com>
2022-08-18 21:10:32 -0700

    Implemented horizontal scroll

--- src/Components/HorizontalScrollBar.tsx
+++ src/Components/HorizontalScrollBar.tsx
@@ -1,6 +1,30 @@
-import { Box } from '@mui/system'
-import React from 'react'
+import { Box, Typography } from '@mui/material'
+import React, { useContext } from 'react';
 import { BodyPartCard } from './BodyPartCard'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import LeftArrowIcon from '../assets/left-arrow.png'
+import RightArrowIcon from '../assets/right-arrow.png'
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
 
 type PropsFromSearch = {
   data: string[]
@@ -8,10 +32,10 @@ type PropsFromSearch = {
   setEachBodyPart: Function
 }
 
-//When I put : React.FC, it generates error
+//Need to fix about scroll
 export const HorizontalScrollBar: React.FC<PropsFromSearch> = ({data, eachBodyPart, setEachBodyPart}) => {
   return (
-    <div>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
         {data.map((bodyPart:any) => (
             <Box
                 key={bodyPart.id || bodyPart}
@@ -21,6 +45,6 @@ export const HorizontalScrollBar: React.FC<PropsFromSearch> = ({data, eachBodyPa
               <BodyPartCard bodyPart={bodyPart} eachBodyPart={eachBodyPart} setEachBodyPart={setEachBodyPart}/>
             </Box>
         ))}
-    </div>
+    </ScrollMenu>
   )
 }
