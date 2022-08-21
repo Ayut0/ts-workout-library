b2159f2fc6f6223350259148c864147d76b48456
Yuto Yamakita <yuvto.0420@gmail.com>
2022-08-21 15:36:48 -0700

    Implemented searching and renderng workout

--- src/Components/Workouts.tsx
+++ src/Components/Workouts.tsx
@@ -1,18 +1,33 @@
 import React, {useEffect, useState} from 'react';
-// import Workout from '../UI/Home'
+import { Workout } from '../UI/Home'
 import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 import { fetchWorkout } from '../utilities/fetchWorkout'
 
 type PropsFromHome ={
-  workouts: ['']
+  workouts: Workout[] | undefined
   setWorkouts: Function
   eachBodyPart: string
   setEachBodyPart: Function
 }
 
-export const Workouts: React.FC<PropsFromHome> = ({ setWorkouts, eachBodyPart, setEachBodyPart }) => {
+export const Workouts: React.FC<PropsFromHome> = ({ workouts, setWorkouts, eachBodyPart, setEachBodyPart }) => {
   return (
-    <div>Workouts</div>
+    <Box id='workouts' sx={{mt: {lg: '110px'}}} mt='50px' p='20px'>
+      <Typography variant='h3' mb='48px' fontFamily='Mochiy Pop One'>
+        Showing results
+      </Typography>
+      <Stack direction='row' sx={{gap: {lg: '110px', xs: '50px'}}} flexWrap='wrap' justifyContent='center'>
+        { (workouts !== undefined) && workouts.map((workout, index) =>(
+          <div>
+            <span>{workout.id}</span>
+            <span>{workout.name}</span>
+            <span>{workout.target}</span>
+            <img src={workout.gifUrl} alt="" />
+            <span>{workout.equipment}</span>
+          </div>
+        ))}
+      </Stack>
+    </Box>
   )
 }
